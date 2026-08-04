104630c25fb49a1d165fd982c4d5810889fa5eef
unknown <lingsuling276@gmail.com>
2026-08-04 08:33:40 +0700

    chore: updating system into alert

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,19 +7,56 @@ use Illuminate\Http\Request;
 class StudentController extends Controller
 {
     public function index() {
-        return view('students.index');
+        $title = 'Sistem Sekolah - Daftar Siswa';
+        $students = [
+            [
+
+            'id' => 1,
+            'nis' => '22100001',
+            'name' => 'Andi',
+            'class' => 'XII TKJ 3',
+            'major' => 'TKJ'
+
+
+            ],
+
+            [
+
+            'id'=> 2,
+            'nis' => '22100002',
+            'name'=> 'Budi',
+            'class'=> 'XII AKL 1',
+            'major' => 'AKL'
+
+            ]
+
+            
+        ];
+        return view('students.index', [
+            'title'=> $title,
+            'students' => $students
+        ]);
     }
 
     public function show(string $id) {
-        return view('students.show');
+        $title = 'Sistem Sekolah - Detail Siswa';  
+        return view('students.show', [
+            'title' => $title,
+        ]);
     }
 
     public function create() {
-        return view('students.create');
+        $title = 'Sistem Sekolah - Tambah Siswa';
+        return view('students.create', [
+            'title' => $title
+        ]);
     }
 
     public function edit(string $id) {
-        return view('students.edit');
+        $title = 'Sistem Sekolah - Edit Siswa';
+        return view('students.edit', [
+            'title' => $title,
+        ]);
     }
 
     public function store () {
